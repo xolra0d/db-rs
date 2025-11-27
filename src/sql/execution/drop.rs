dfb3ec221facf1e1604745e997a816702c0a3394
Serhii Nikolaiev <sn24008@edu.lu.lv>
2025-11-27 08:20:28 +0200

    added drop command (#8)

---
+++ src/sql/execution/drop.rs
@@ -0,0 +1,56 @@
+use crate::config::CONFIG;
+use crate::error::{Error, Result};
+use crate::runtime_config::TABLE_DATA;
+use crate::sql::CommandRunner;
+use crate::storage::{OutputTable, TableDef};
+
+impl CommandRunner {
+    pub fn drop_table(table_def: &TableDef, if_exists: bool) -> Result<OutputTable> {
+        let lock = TABLE_DATA.remove(table_def);
+
+        if lock.is_none() && if_exists {
+            return Ok(OutputTable::build_ok());
+        } else if lock.is_none() && !if_exists {
+            return Err(Error::TableNotFound);
+        }
+        let Some(lock_data) = lock else {
+            unreachable!()
+        };
+
+        let remove_result = std::fs::remove_dir_all(lock_data.0.get_path());
+        match (remove_result, if_exists) {
+            (Ok(()), _) => Ok(OutputTable::build_ok()),
+            (Err(error), true) if error.kind() == std::io::ErrorKind::NotFound => {
+                Ok(OutputTable::build_ok())
+            }
+            (Err(error), false) if error.kind() == std::io::ErrorKind::NotFound => {
+                Err(Error::TableNotFound)
+            }
+            (Err(error), _) => Err(Error::Internal(format!(
+                "Could not remove table entry from disk: {}. Stop database, remove {:?} folder, and restart the database.",
+                error,
+                std::path::absolute(table_def.get_path())
+            ))),
+        }
+    }
+
+    pub fn drop_database(name: &str, if_exists: bool) -> Result<OutputTable> {
+        TABLE_DATA.retain(|x, _| x.database != name);
+
+        let remove_result = std::fs::remove_dir_all(CONFIG.get_db_dir().join(name));
+        match (remove_result, if_exists) {
+            (Ok(()), _) => Ok(OutputTable::build_ok()),
+            (Err(error), true) if error.kind() == std::io::ErrorKind::NotFound => {
+                Ok(OutputTable::build_ok())
+            }
+            (Err(error), false) if error.kind() == std::io::ErrorKind::NotFound => {
+                Err(Error::DatabaseNotFound)
+            }
+            (Err(error), _) => Err(Error::Internal(format!(
+                "Could not remove database entry from disk: {}. Stop database, remove {:?} folder, and restart the database.",
+                error,
+                std::path::absolute(CONFIG.get_db_dir().join(name))
+            ))),
+        }
+    }
+}
